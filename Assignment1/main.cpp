f0842f0d1cef811dd62b5b224656e133fc1040a1
GZakharov1525 <GZakharov1525@users.noreply.github.com>
2017-09-15 09:59:22 -0400

    Basic I/O code
    Answered your question Justin, added basic input for the vertices with error checking

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -1,3 +1,7 @@
+// sofe3770uAssign1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
 #include <iostream>
 #include <vector>
 #include "geometry.hpp"
@@ -6,8 +10,59 @@ using namespace std;
 
 int main()
 {
-    Point a;
-    cout << "The co-ordinates of a are " << a.x << " & " << a.y << "\n";
-    cout << "Why are they both 0 GEORGE ?\n";
-    return 0;
+	// Declaring and Initializng variables
+	int polygonVertices;
+	vector<int> xPoints;
+	vector<int> yPoints;
+
+	/*
+	Point a;
+	cout << "The co-ordinates of a are " << a.x << " & " << a.y << "\n";
+	cout << "Why are they both 0 GEORGE ?\n"; // Because the point object has a constructor that has default values.
+											 // See here: http://sandbox.mc.edu/~bennet/cs220/codeex/cl0_cc.html
+	*/
+
+	cout << "Please enter the number of Vertices (3 < _ < 200): ";
+	cin >> polygonVertices;
+	// Error checking for incorrect input
+	while (polygonVertices > 200 || polygonVertices < 3)
+	{
+		cout << "Please enter a valid number: ";
+		cin >> polygonVertices;
+	}
+	/*for (int xTemp, yTemp; cin >> xTemp >> yTemp;)
+	{
+		xPoints.push_back(xTemp);
+		yPoints.push_back(yTemp);
+	}*/
+	cout << "Enter the vertices co-ordinates starting with x then y for each pair in a counter-clockwise order.";
+	cout << "Please enter integers equal to or less than 10^6.";
+	for (int count = 0; count < 7; count++)
+	{
+		int xTemp;
+		cin >> xTemp;
+		xPoints.push_back(xTemp);
+		if (xTemp > 1000000)
+		{
+			cout << "Error";
+			cin >> xTemp;
+		}
+		int yTemp;
+		cin >> yTemp;
+		yPoints.push_back(yTemp);
+		if (yTemp > 1000000)
+		{
+			cout << "Error";
+			cin >> yTemp;
+		}
+	}
+
+
+
+	// keeps window open
+	int dummy;
+	cin >> dummy; 
+	
+	return 0;
 }
+
