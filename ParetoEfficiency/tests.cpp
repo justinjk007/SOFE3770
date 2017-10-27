1f753c9a3d14f7ab5c71c82f075a51d8dbdb4dff
GZakharov1525 <GZakharov1525@users.noreply.github.com>
2017-10-27 15:56:00 -0400

    added vector sets

--- ParetoEfficiency/tests.cpp
+++ ParetoEfficiency/tests.cpp
@@ -120,3 +120,26 @@ TEST_CASE("Testing Integration high low")
     print(frontier);
     draw(points, frontier);
 }
+
+TEST_CASE("Sets of Point Vectors")
+{
+    // Set 1
+    vector<int> uValues = {0, 12, 2, 5, 8, 12, 8, 4, 3, 10, 11, 6, 2, 4, 8, 10, 10, 3, 2, 6};
+    vector<int> vValues = {0, 1, 2, 2, 2, 3, 4, 4, 5, 5, 6, 6, 8, 8, 8, 8, 10, 10, 12, 11};
+    // convex hull values should be 0,0  12,1  12,3  11,6  10,10  6,11  2,12
+
+    // Set 2
+    vector<int> xValues = {0, 2, 2, 3, 4, 4, 4, 5, 6, 6, 6, 7, 8, 8, 8, 8, 9, 10, 10, 11};
+    vector<int> yValues = {0, 5, 10, 8, 3, 6, 9, 7, 1, 5, 8, 7, 4, 6, 9, 10, 5, 2, 7, 3};
+    // convex hull values should be 0,0  6,1  10,2  11,3  10,7  8,10  2,10
+
+    // Set 3
+    vector<int> aValues = {0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5, 5, 7, 8, 9, 10};
+    vector<int> bValues = {0, 2, 4, 3, 5, 7, 1, 2, 4, 10, 3, 4, 1, 3, 6, 8, 5, 2, 8, 4};
+    // convex hull values should be 0,0  3,1  5,1  8,2  10,4  9,8  5,8  3,10  2,7  1,4
+
+    // Set 4
+    vector<int> sValues = {0, 2, 2, 3, 3, 5, 5, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 10, 10, 11};
+    vector<int> tValues = {0, 9, 10, 2, 5, 5, 7, 4, 6, 3, 5, 7, 8, 3, 4, 5, 2, 3, 5, 2};
+    // convex hull values should be 0,0  11,2  10,5  7,8  2,10
+}
